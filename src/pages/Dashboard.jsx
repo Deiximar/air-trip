148a75eb36f4938058a99769d3af7a261d9d2711
PabloA1A <pablo_abadalvarez@hotmail.es>
2024-07-04 16:37:10 +0200

    installed and configured vitest. First test of City.jsx component passed ok

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,8 +1,10 @@
+import City from "../components/button/City.jsx";
 import Country from "../components/button/Country.jsx";
 
 const Dashboard = () => {
   return <div>
     <Country />
+    <City />
   </div>;
 };
 
