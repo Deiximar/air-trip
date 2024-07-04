4529ffe7af237904c261f90fd00e3d0dc54bf70f
PabloA1A <pablo_abadalvarez@hotmail.es>
2024-07-04 12:27:02 +0200

    finished city button that takes the data of all the cities of the world from a json file and the user selects it with a drop-down from the list of cities

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,8 +1,14 @@
+import City from "../components/button/City";
 import Country from "../components/button/Country";
 
 const Dashboard = () => {
-  return <div>Soy Dashboard</div>;
-  <Country />
+  return (
+    <div>
+      Soy Dashboard
+      <Country />
+      <City />
+    </div>
+  );
 };
 
 export default Dashboard;
